284652f6daa7b41eae24c3f9343ad7c46804a764
Hossein Margani <hossein@margani.dev>
2023-12-11 11:51:02 +0000

    Change names and files structure

--- src/modules/lib.psm1
+++ src/modules/lib.psm1
@@ -1,4 +1,4 @@
-﻿Import-Module ./persian-lib.psm1 -Force
+﻿Import-Module ./src/modules/persian-lib.psm1 -Force
 
 $PersianCalendar = New-Object System.Globalization.PersianCalendar
 $Colors = @('#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02', '#a6761d', '#666666')
---
+++ src/tgju/update-daily.ps1
@@ -0,0 +1,10 @@
+Import-Module ./src/modules/lib.psm1 -Force
+Import-Module ./src/modules/persian-lib.psm1 -Force
+
+$dataRootPath = Join-Path "./" "tgju" "current"
+
+Update-PriceDB -DataRootPath $dataRootPath
+Add-Charts -Keys @(
+    'price_dollar_rl', 'price_gbp', 'price_eur', 'mesghal',
+    'retail_sekee', 'retail_sekeb', 'retail_nim', 'retail_rob', 'retail_gerami'
+)
