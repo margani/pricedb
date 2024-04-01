b4ce68540c2784ccb55c57940570db0786143dff
Hossein Margani <hossein@margani.dev>
2024-04-01 14:06:12 +0100

    Add history API

---
+++ netlify/functions/v1-history/index.mjs
@@ -0,0 +1,40 @@
+import { getDataSource, getLastXDays, parseDate } from "../../utils";
+
+export default async (request, context) => {
+  try {
+    const { symbol, base } = context.params;
+    const requestUrl = new URL(request.url);
+    const from = requestUrl.searchParams.get("from");
+    const to = requestUrl.searchParams.get("to");
+
+    const dataSource = getDataSource(symbol, base);
+    if (!dataSource) {
+      return Response.json(
+        { error: `No exchange rate found for [${symbol}] and [${base}]` },
+        { status: 404 }
+      );
+    }
+
+    let fromDate = parseDate(from, getLastXDays(30));
+    let toDate = parseDate(to, new Date());
+
+    const dataUrl = dataSource.mapping(dataSource[symbol]);
+    const dataResponse = await fetch(`${dataUrl}/history.json`);
+    const data = await dataResponse.json();
+
+    const result = data
+      .filter((_) => parseDate(_.ts) >= fromDate && parseDate(_.ts) <= toDate)
+      .map(dataSource.transform);
+
+    return Response.json({
+      success: true,
+      result,
+    });
+  } catch (error) {
+    return Response.json({ error: "Failed fetching data" }, { status: 500 });
+  }
+};
+
+export const config = {
+  path: "/v1/chart/:base/:symbol",
+};
