86a3ca73c21a1fbb8ab7e53f976a6cba7605ae3f
Hossein Margani <margani@users.noreply.github.com>
2024-08-27 16:34:25 +0100

    Try parsing the time again

--- src/utils/index.js
+++ src/utils/index.js
@@ -56,10 +56,10 @@ export async function getHistory(dataSource, base, symbol, from, to) {
   const symbolData = (await SymbolDataResponse.json())
     .filter((_) => parseDate(_.ts) >= fromDate && parseDate(_.ts) <= toDate)
     .map(dataSource.transform)
-    // .map((_) => ({
-    //   ..._,
-    //   time: _.time.Date
-    // }));
+    .map((_) => ({
+      ..._,
+      time: new Date(Date.parse(_.time)).Date
+    }));
 
   let data = [];
   if (base === "irr") {
@@ -70,10 +70,10 @@ export async function getHistory(dataSource, base, symbol, from, to) {
     const baseData = (await baseDataResponse.json())
       .filter((_) => parseDate(_.ts) >= fromDate && parseDate(_.ts) <= toDate)
       .map(dataSource.transform)
-      // .map((_) => ({
-      //   ..._,
-      //   time: _.time.Date
-      // }));
+      .map((_) => ({
+        ..._,
+        time: new Date(Date.parse(_.time)).Date
+      }));
 
     baseData.forEach(item => {
       const symbolItem = symbolData.filter(_=>_.time === item.time)
